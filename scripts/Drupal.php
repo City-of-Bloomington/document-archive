be869e85fb8f7dbd67ca0bf5fc0c36f69ee1f5c6
Cliff Ingham <inghamn@bloomington.in.gov>
2026-03-09 16:41:55 -0400

    Create Drupal class for content updating
    Updates #2

---
+++ scripts/Drupal.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * @copyright 2026 City of Bloomington, Indiana
+ * @license https://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
+ */
+declare (strict_types=1);
+
+class Drupal
+{
+    public \PDO $pdo;
+
+    public function __construct(array $config)
+    {
+        $this->pdo = new \PDO($config['dsn'], $config['user'], $config['pass']);
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+
+    function update_links(array $file, int $archive_id)
+    {
+        echo "Update links to archive_id: $archive_id\n";
+        $encoded = rawurlencode($file['filename']);
+        $url     = "/archive?origin_id=$file[fid]&archive_id=$archive_id&filename=$encoded";
+        $encoded = str_replace('%', '\%', $encoded);
+
+        $content_tables = [
+            'node__body'                 => 'body_value',
+            'node__field_aside'          => 'field_aside_value',
+            'node__field_details'        => 'field_details_value',
+        ];
+        $links_tables = [
+            'node__field_related_links'  => 'field_related_links_uri',
+            'node__field_call_to_action' => 'field_call_to_action_uri',
+            'paragraph__field_info_link' => 'field_info_link_uri'
+        ];
+
+        foreach ($content_tables as $table=>$field) {
+
+            $this->replace_links_in_html($table, $field, $file['filename'], $url);
+            $this->replace_links_in_html($table, $field, $encoded,          $url);
+        }
+        foreach ($links_tables as $table=>$field) {
+            $this->replace_links_in_url_fields($table, $field, $file, $url);
+        }
+    }
+
+    function replace_links_in_html(string $table, string $field, string $filename, string $url)
+    {
+        $host  = 'https\:\/\/bloomington\.in\.gov';
+        $path  = '\/sites\/default\/files\/[[:digit:]]{4}-[[:digit:]]{2}';
+
+        $regex = "href=\"(($host)?$path\/$filename)\"";
+        $sql   = "select entity_id, $field from $table where regexp_like($field, ?)";
+        $query = $this->pdo->prepare($sql);
+        $query->execute([$regex]);
+        foreach ($query->fetchAll(\PDO::FETCH_ASSOC) as $r) {
+            $html   = preg_replace("/$regex/", "href=\"$url\"", $r[$field]);
+            $sql    = "update $table set $field=? where entity_id=?";
+            $update = $this->pdo->prepare($sql);
+            echo "Updating $table for entity_id: $r[entity_id]\n";
+            $success = $update->execute([$html, $r['entity_id']]);
+            if (!$success) {
+                echo "Failed: $sql\n";
+                exit();
+            }
+        }
+    }
+
+    function replace_links_in_url_fields(string $table, string $field, array $file, string $url)
+    {
+        $original = 'https://bloomington.in.gov/sites/default/files'.$file['path'];
+        $dir      = dirname($original);
+        $encoded  = $dir.'/'.str_replace('%', '\%', rawurlencode($file['filename']));
+        echo "Replacing $table: $original or $encoded\n";
+
+        $sql      = "update $table set $field=? where $field=? or $field=?";
+        $update   = $this->pdo->prepare($sql);
+        $success  = $update->execute([$url, $original, $encoded]);
+        if (!$success) {
+            echo "Failed: $sql\n";
+            exit();
+        }
+    }
+}
---
+++ scripts/import_drupal.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * @copyright 2026 City of Bloomington, Indiana
+ * @license https://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
+ */
+declare (strict_types=1);
+define('DRUPAL_HOME', '/srv/data/drupal');
+define('SITE_HOME', $_SERVER['SITE_HOME']);
+include SITE_HOME.'/site_config.php';
+include './Drupal.php';
+
+$importer = new Import($DATABASES['default']);
+$drupal   = new Drupal($DATABASES['drupal' ]);
+
+$sql     = "select f.fid,
+                   f.filename,
+                   f.filemime,
+                   replace(f.uri, 'public:/', '') as path,
+                   from_unixtime(f.created)       as created,
+                   du.name                        as username,
+                   wu.department
+            from      file_managed         f
+                 join users_field_data    du on  f.uid=du.uid
+                 join wave.users          wu on du.name=wu.username
+            left join wave.grackle_results g on replace(f.uri, 'public:/', '')=replace(g.url, 'https://bloomington.in.gov/sites/default/files', '')
+            where f.filemime like 'application/pdf'
+              and (g.score is null or g.score<90)";
+$query   = $drupal->pdo->query($sql);
+$files   = $query->fetchAll(\PDO::FETCH_ASSOC);
+foreach ($files as $f) {
+    echo "$f[fid] $f[path]\n";
+
+    $archive_id = $importer->import_file($f);
+    echo "archive_id: $archive_id\n";
+    $drupal->update_links($f, $archive_id);
+    echo "-------------------------------------------------\n";
+}
+
+class Import
+{
+    public \PDO          $pdo;
+    public \PDOStatement $insert;
+
+    public function __construct(array $config)
+    {
+        $this->pdo = new \PDO($config['dsn'], $config['user'], $config['pass']);
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+        $fields = [
+            'internalFilename',
+            'filename',
+            'mime_type',
+            'md5',
+            'uploaded',
+            'username',
+            'department',
+            'origin',
+            'origin_id'
+        ];
+        $col    = implode(',', $fields);
+        $par    = implode(',', array_map(fn($f): string => ":$f", $fields));
+        $this->insert = $this->pdo->prepare("insert into files ($col) values($par)");
+    }
+
+    public function import_file(array $file): int
+    {
+        $original  = DRUPAL_HOME.'/files'.$file['path'];
+        $md5       = md5_file($original);
+        if (!$md5) {
+            echo "No such file: $original\n";
+            exit();
+        }
+
+        $uploaded  = new \DateTime($file['created']);
+        $ym        = $uploaded->format('Y/m');
+        $internal  = "/$ym/".uniqid();
+
+        $directory = SITE_HOME.'/files';
+        if (!is_dir("$directory/$ym")) {
+            mkdir  ("$directory/$ym", 0775, true);
+        }
+        copy($original, $directory.$internal);
+
+        $d = [
+            'origin'           => 'drupal',
+            'internalFilename' => $internal,
+            'md5'              => $md5,
+            'origin_id'        => $file['fid'       ],
+            'filename'         => $file['filename'  ],
+            'mime_type'        => $file['filemime'  ],
+            'uploaded'         => $file['created'   ],
+            'username'         => $file['username'  ],
+            'department'       => $file['department'],
+        ];
+        $this->insert->execute($d);
+        print_r($d);
+        $archive_id = (int)$this->pdo->lastInsertId();
+        return $archive_id;
+    }
+}
